867d7928183fd12d77a369c5449b7269a0d6e4d3
Wali <humaidwali20@gmail.com>
2025-03-03 12:00:31 +0530

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'no-var': 'off',
     },
   },
 ];
